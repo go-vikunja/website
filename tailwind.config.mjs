2ef68e5fe2aa9cf22363732f1c9a75d90f74d099
kolaente <k@knt.li>
2024-06-20 13:24:44 +0200

    feat: basic layout with navigation and footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					lighter: '#3b7aec',
+					DEFAULT: '#196aff',
+					darker: '#0064ff',
+				},
+			},
+			fontFamily: {
+				title: ['Quicksand', ...defaultTheme.fontFamily.sans],
+				sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+			},
+			backgroundImage: {
+				'hero-llama': "url('/images/bg-3.jpg')",
+				'hero-llama-small': "url('/images/hero-llamas-small.jpg')",
+				'hero-llama-2-small': "url('/images/hero-llamas-2-small.jpg')",
+				'hero-llama-3-small': "url('/images/hero-llamas-3-small.jpg')",
+			},
+		},
 	},
 	plugins: [],
 }
