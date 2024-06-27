152b5e82d62927ca2125fada365b33be46b97804
kolaente <k@knt.li>
2024-06-27 16:25:19 +0200

    feat: add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import markdoc from "@astrojs/markdoc";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), markdoc()]
+  output: 'hybrid',
+  integrations: [tailwind(), markdoc()],
+  adapter: cloudflare()
 });
