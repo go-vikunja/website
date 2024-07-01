4e9f00ea518a00d41340e4a20a6ced5ded2ee354
kolaente <k@knt.li>
2024-07-01 21:45:35 +0200

    fix: set correct base site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://vikunja.io',
 	output: 'hybrid',
 	integrations: [tailwind(), markdoc()],
 	adapter: node({
