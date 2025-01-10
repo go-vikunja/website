0aff360d43caf9d6f5b979a78e6ee8a9c6ccb21e
kolaente <k@knt.li>
2025-01-10 12:53:53 +0100

    feat: deploy on cloudflare pages

--- devenv.nix
+++ devenv.nix
@@ -3,6 +3,10 @@
 let
   unstable = import inputs.unstable { system = pkgs.stdenv.system; };
 in {
+  scripts.patch-workerd.exec = ''
+  find node_modules/.pnpm/@cloudflare+workerd-linux-64@*/node_modules/@cloudflare/workerd-linux-64/bin/ -name workerd -print0 | xargs -I {} -0 patchelf --set-interpreter "$(<$NIX_CC/nix-support/dynamic-linker)" {}
+  '';
+
   languages.javascript = {
     enable = true;
     pnpm = {
