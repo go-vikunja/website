c85f7584c85499eb4587f97350e8e8d36678bdfc
kolaente <k@knt.li>
2025-04-01 12:40:55 +0200

    fix(helper): format single-digit days correctly

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,4 +1,5 @@
 export function formatDate(date: Date): string {
 	const month = ('0' + (date.getMonth() + 1)).slice(-2)
-	return date.getFullYear() + '-' + month + '-' + date.getDate()
+	const day = ('0' + date.getDate()).slice(-2)
+	return date.getFullYear() + '-' + month + '-' + day
 }
