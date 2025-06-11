c4a7674ceaf2f8ccb5ef65395046fa9c0e5e2b2b
Dominik Pschenitschni <6173598+dpschen@users.noreply.github.com>
2025-06-11 22:19:48 +0200

    Make Vikunja cloud link configurable

--- src/helpers/vikunjaCloudLink.ts
+++ src/helpers/vikunjaCloudLink.ts
@@ -1,1 +1,4 @@
-export const VIKUNJA_CLOUD_LINK = 'https://app.vikunja.cloud/?redirectToProvider=true'
+const DEFAULT_VIKUNJA_CLOUD_LINK = 'https://app.vikunja.cloud/?redirectToProvider=true'
+
+export const VIKUNJA_CLOUD_LINK =
+       process.env.VIKUNJA_CLOUD_URL ?? DEFAULT_VIKUNJA_CLOUD_LINK
