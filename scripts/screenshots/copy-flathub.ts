3ef6e9a02263fc06b9c700a851d1ec0ebc230802
kolaente <k@knt.li>
2026-03-09 18:24:48 +0100

    refactor: remove legacy dark screenshot, update flathub copy script

--- scripts/screenshots/copy-flathub.ts
+++ scripts/screenshots/copy-flathub.ts
@@ -7,5 +7,5 @@ const vikunjaDir = join(__dirname, '../../src/assets/images/vikunja')
 const flathubDir = join(__dirname, '../../public/flathub-images')
 
 cpSync(join(vikunjaDir, '01-task-overview.png'), join(flathubDir, '01-task-overview.png'))
-// 09-task-detail-dark.png will be added when dark mode screenshot is implemented
+cpSync(join(vikunjaDir, '01-task-overview-dark.png'), join(flathubDir, '01-task-overview-dark.png'))
 console.log('Flathub images copied.')
